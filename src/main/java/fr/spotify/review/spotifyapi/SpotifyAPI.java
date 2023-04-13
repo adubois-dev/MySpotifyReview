e1d4c4ab2f4a287bd555f829f094008da1f88aea
adubois <adubois.personnel@gmail.com>
2023-04-13 08:15:59 +0200

    Refonte complète du code en mode REST

--- src/main/java/fr/spotify/review/spotifyapi/SpotifyAPI.java
+++ src/main/java/fr/spotify/review/spotifyapi/SpotifyAPI.java
@@ -9,8 +9,8 @@ public class SpotifyAPI {
 
     public static void spotifyQuery(){
         SpotifyApi spotifyApi = new SpotifyApi.Builder()
-                .setClientId("1faf3b88a6233c43787a3495545227f93dd")
-                .setClientSecret("e3d875b3c5580546c62a0219184df66713bf")
+                .setClientId("1faf3b8a623c4387a3495545227f93dd")
+                .setClientSecret("e3d85b3c55054662a021918df66713bf")
                 .setRedirectUri(URI.create("http://localhost:12345/callback"))
                 .build();
 
